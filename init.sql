2da1f219e2de8dd1507eadf404f1eeefcc4a5fab
JM314155 <muurimakijere4@gmail.com>
2026-03-29 22:52:23 +0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,6 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('helsinki', 'paikat');
+INSERT INTO classes (title, value) VALUES ('espoo', 'paikat');
+INSERT INTO classes (title, value) VALUES ('vantaa', 'paikat');
+INSERT INTO classes (title, value) VALUES ('tampere', 'paikat');
